c8c624ce63134a021e554d6de66fb34e11f33dc4
Olha Kozmenkova <akozmenkova@ukr.net>
2026-04-28 22:54:30 +0200

    feat(advert): add advert modul

--- src/advert/advert.repository.ts
+++ src/advert/advert.repository.ts
@@ -5,18 +5,20 @@ import { Advert } from './advert.interface';
 export class AdvertRepository {
   private adverts: Advert[] = [
     {
-      id: 1,
+      id: 0,
       image: 'https://example.com/banner1.jpg',
       content: 'Купите наш продукт со скидкой 50%',
       link: 'https://example.com/product',
     },
     {
-      id: 2,
+      id: 1,
       image: 'https://example.com/banner2.jpg',
       content: 'Новая коллекция уже в продаже',
       link: 'https://example.com/collection',
     },
   ];
+
+  private idCounter = 2;
 
   findAll(): Advert[] {
     return this.adverts;
